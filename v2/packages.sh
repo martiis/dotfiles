a376741857e2984fb0a2e1f50ca556775fe7766a
Martynas Sudintas <martynas.s@eneba.com>
2026-02-02 23:42:01 +0200

    feat: add core tools and zsh configuration

--- v2/packages.sh
+++ v2/packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eu
 
@@ -15,6 +15,9 @@ info() {
 warn() {
     printf "${YELLOW}==>${NC} %s\n" "$1"
 }
+
+info "Installing core tools (eza, bat, fzf, zoxide)..."
+yay -S --needed --noconfirm eza bat fzf zoxide
 
 info "Installing tmux..."
 yay -S --needed --noconfirm tmux
@@ -41,4 +44,52 @@ if [ "$SHELL" != "$(which zsh)" ]; then
     chsh -s "$(which zsh)"
 else
     warn "zsh is already the default shell."
+fi
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    info "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    info "Installing zsh-syntax-highlighting..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+ZSHRC="$HOME/.zshrc"
+info "Updating plugins in .zshrc..."
+if grep -q '^plugins=(' "$ZSHRC"; then
+    sed -i 's/^plugins=(.*/plugins=(git docker docker-compose zsh-autosuggestions zsh-syntax-highlighting)/' "$ZSHRC"
+else
+    echo 'plugins=(git docker docker-compose zsh-autosuggestions zsh-syntax-highlighting)' >> "$ZSHRC"
+fi
+
+info "Adding aliases to .zshrc..."
+ALIASES=(
+    "alias ls='eza -lh --group-directories-first --icons=auto'"
+    "alias lt='eza --tree --level=2 --long --icons --git'"
+    "alias cat='bat'"
+    "alias ..='cd ..'"
+    "alias ...='cd ../..'"
+    "alias ....='cd ../../..'"
+)
+
+for alias_line in "${ALIASES[@]}"; do
+    if ! grep -Fq "$alias_line" "$ZSHRC"; then
+        echo "$alias_line" >> "$ZSHRC"
+    fi
+done
+
+info "Configuring .zshrc for fzf and zoxide..."
+if [ ! -f "$ZSHRC" ]; then
+    touch "$ZSHRC"
+fi
+
+if ! grep -Fxq "source <(fzf --zsh)" "$ZSHRC"; then
+    echo "source <(fzf --zsh)" >> "$ZSHRC"
+fi
+
+if ! grep -Fxq 'eval "$(zoxide init --cmd cd zsh)"' "$ZSHRC"; then
+    echo 'eval "$(zoxide init --cmd cd zsh)"' >> "$ZSHRC"
 fi
